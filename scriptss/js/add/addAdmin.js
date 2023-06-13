30c7ff8eca0bbf35fb6ec0180a4e68a51de2c19b
Once <98696858+EvAyako@users.noreply.github.com>
2023-06-13 21:39:37 +0300

    Add files via upload

---
+++ scriptss/js/add/addAdmin.js
@@ -0,0 +1,4 @@
+const addAdms = new addItems();
+
+addAdms.addAdmin('Main', 'Owner, Site Developer', './imgs/adms/main.png', 'Once#7474');
+addAdms.addAdmin('XVIII', 'Owner, Client Developer', './imgs/adms/apogee.png', 'ApogeeUwU#0069');
