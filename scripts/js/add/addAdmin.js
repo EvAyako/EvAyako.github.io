28f0e5d0f3cc7219a3ff6889e32aeb053591040c
Once <98696858+EvAyako@users.noreply.github.com>
2023-06-13 21:38:15 +0300

    Delete scripts/js directory

--- scripts/js/add/addAdmin.js
+++
@@ -1,4 +0,0 @@
-const addAdms = new addItems();
-
-addAdms.addAdmin('Main', 'Owner, Site Developer', './imgs/adms/main.png', 'Once#7474');
-addAdms.addAdmin('XVIII', 'Owner, Client Developer', './imgs/adms/apogee.png', 'ApogeeUwU#0069');
