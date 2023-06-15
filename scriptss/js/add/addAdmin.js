681e6decc03b0175014e4fe75f65baf8e96a8559
ApogeeUwU <artgrartic@gmail.com>
2023-06-15 21:24:30 +0500

    a

--- scriptss/js/add/addAdmin.js
+++ scriptss/js/add/addAdmin.js
@@ -1,4 +1,3 @@
 const addAdms = new addItems();
-
 addAdms.addAdmin('Main', 'Owner, Site Developer', './imgs/adms/main.png', 'Once#7474');
-addAdms.addAdmin('XVIII', 'Owner, Client Developer', './imgs/adms/apogee.png', 'ApogeeUwU#0069');
+addAdms.addAdmin('XVIII', 'Owner, Client Developer', './imgs/adms/apogee.png', 'ApogeeUwU#0001');
