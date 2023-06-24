6e25d0ecde89c0c53d1736d2c80d7edb0d8f3565
Once <98696858+EvAyako@users.noreply.github.com>
2023-06-24 13:22:39 +0300

    Update addAdmin.js

--- scriptss/js/add/addAdmin.js
+++ scriptss/js/add/addAdmin.js
@@ -1,3 +1,4 @@
 const addAdms = new addItems();
-addAdms.addAdmin('Main', 'Owner, Site Developer', './imgs/adms/main.png', 'Once#7474');
-addAdms.addAdmin('XVIII', 'Owner, Client Developer', './imgs/adms/apogee.png', 'ApogeeUwU#0001');
+addAdms.addAdmin('Main', 'Owner, Site Developer', './imgs/adms/main.png', 'reqonce');
+addAdms.addAdmin('XVIII', 'Owner, Client Developer', './imgs/adms/apogee.png', 'apogeeuwu');
+addAdms.addAdmin('landyshcoollegends', 'Elder Moderator', './imgs/adms/cool.png', 'landyshcoollegends');
